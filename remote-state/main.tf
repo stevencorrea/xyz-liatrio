49fb29ceec9db2a9fece978028460d19ed6db64b
Steven Correa <stevencorrea@users.noreply.github.com>
2023-02-21 19:38:17 +0000

    use aws cred action

--- remote-state/main.tf
+++ remote-state/main.tf
@@ -30,7 +30,7 @@ resource "aws_dynamodb_table" "terraform_state_lock" {
   }
 }
 
-resource "aws_s3_bucket_policy" "terraform_state_encrypt" {
+resource "aws_s3_bucket_policy" "terraform_state_encryption" {
   bucket = "${aws_s3_bucket.terraform_state.id}"
   policy =<<EOF
 {
