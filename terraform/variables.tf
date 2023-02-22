db6aff2a37601f30643033c7dde307a7890c73cd
Steven Correa <stevencorrea@users.noreply.github.com>
2023-02-22 11:59:50 -0800

    chore: test eks deploy to us-west-2 (#9)

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -4,15 +4,16 @@ variable "name" {
   default     = "xyz-liatrio"
 }
 
+variable "region" {
+  description = "Region for deploy"
+  type        = string
+  default     = "us-west-2"
+
+}
+
 variable "cluster_version" {
   description = "Version of k8s for EKS"
   type        = string
   default     = "1.24"
 
-}
-
-variable "azs" {
-  description = "Availability Zones to deploy nodes into"
-  type        = list(any)
-  default     = ["us-west-2a", "us-west-2b", "us-west-2c"]
 }
