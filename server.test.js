ebff1f305b13f83b45d42fb2845fe0c95b26b606
Steven Correa <stevencorrea@users.noreply.github.com>
2023-02-22 22:48:33 -0800

    feat: add comment to test (#23)

--- server.test.js
+++ server.test.js
@@ -3,6 +3,7 @@ const assert = require('assert')
 const app = require('./server')
 
 // This test ensures the endpoint returns a 200, json and has a status and message properties
+// TODO: Add a test of the content itself?
 describe('Testing XYZ Endpoint', function() {
     it('GETs the endpoint', function(done) {
         request('http://127.0.0.1:3001')
